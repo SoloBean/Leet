80b796140bc21a422a726ba1f82382ad40b45939
SoloBean <1138260878@qq.com>
2017-08-08 15:59:19 +0800

    Initial commit

---
+++ src/sameTree/SameTree.java
@@ -0,0 +1,27 @@
+package sameTree;
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return front(p, q);
+    }
+
+    public boolean front(TreeNode p, TreeNode q){
+        if (p == null && q == null){
+            return true;
+        }
+        else if (p == null || q == null){
+            return false;
+        }
+        int a = p.val;
+        int b = q.val;
+        if (a != b){
+            return false;
+        }
+        if (front(p.left, q.left)){
+            if (front(p.right, q.right)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
